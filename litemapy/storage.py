4f101939c4f9b9fc863a32febca116df83c79625
SmylerMC <smyler@mail.com>
2020-03-23 14:18:44 +0100

    Implemented LitematicaBitArray reading

--- litemapy/storage.py
+++ litemapy/storage.py
@@ -14,13 +14,38 @@ class LitematicaBitArray:
         #TODO Check if size is compatible with long array length
         buff = b''
         for l in arr:
-            buff += struct.pack('<Q', int(l)) #TODO Make sure this is right
+            buff += struct.pack('<q', int(l)) #TODO Make sure this is right
         r = LitematicaBitArray(size, nbits)
         r.store = buff
+        #print(buff)
         return r
 
-    def __getitem__(self, key):
-        pass #TODO
+    def __getitem__(self, index):
+        startoff = index * self.nbits
+        startind = startoff // 8
+        endoff = (index + 1) * self.nbits
+        endind = ceil(endoff / 8)
+        sbitoff = startoff % 8
+        ebitoff = endoff % 8
+        #print(startoff, startind, endoff, endind, sbitoff, ebitoff)
+        v = 0
+        for i, b in enumerate(self.store[startind: endind]):
+            bitadded = 8
+            #print(b)
+            if i == endind - startind - 1:
+                b >>= (8 - ebitoff) % 8
+                bitadded -= (8 - ebitoff) % 8
+                #print(b, bitadded)
+            if i == 0:
+                b &= (1 << (8-sbitoff)) - 1
+                bitadded -= sbitoff
+                #print(b, bitadded)
+            #print(b)
+            #print()
+            v <<= bitadded
+            #print(bitadded)
+            v += b
+        return v
 
     def __setitem__(self, key, value):
         pass #TODO
