35e78ab8d5892a42364b44248a165717fa3ca7d3
SmylerMC <smyler@mail.com>
2020-03-31 23:41:43 +0200

    Added an example

---
+++ examples/example1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+from litemapy.schematic import Schematic, Region, BlockState
+
+
+# Creating a schematic object and attaching it a region
+schem = Schematic(21, 21, 21, name="Planet", author="SmylerMC", description="Made with litemapy")
+reg = Region(0, 0, 0, 21, 21, 21, name="planet")
+schem.regions.append(reg)
+
+# Create the block state we are going to use (this is mutable)
+block = BlockState("minecraft:light_blue_concrete")
+
+# Build the planet
+for x in range(21):
+    for y in range(21):
+        for z in range(21):
+            if round(((x-10)**2 + (y-10)**2 + (z-10)**2)**.5) <= 10:
+                reg.setblock(x, y, z, block)
+
+# Save the schematic
+schem.save("planet.litematic")
+
+# Load the schematic and get its first region
+schem = Schematic.load("planet.litematic")
+reg = schem.regions[0]
+
+# Get the range where to loop, width, height and length
+# could be negative depending on the orientation of the build
+xran = [reg.width, 0]
+xran.sort()
+yran = [reg.height, 0]
+yran.sort()
+zran = [reg.length, 0]
+zran.sort()
+sx, ex = xran
+sy, ey = yran
+sz, ez = zran
+
+# Print out the basic shape
+for x in range(sx, ex):
+    for z in range(sz, ez):
+        b = reg.getblock(x, 10, z)
+        if b.blockid == "minecraft:air":
+            print(" ", end="")
+        else:
+            print("#", end='')
+    print()
+
+
