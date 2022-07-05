313c0e4ec8beb27b37e3a5da276ac99b70294e04
SmylerMC <smyler@mail.com>
2022-07-05 19:26:49 +0200

    Refactor tests to use pytest instead of unittest

--- tests/test_boxes.py
+++ tests/test_boxes.py
@@ -1,27 +1,26 @@
-import unittest
 import litemapy.boxes as boxes
 
-class TestBoxes(unittest.TestCase):
 
-    def setUp(self):
-        self.origin = (0, 0, 0)
-        self.block1 = (-2, -2, -2)
-        self.block2 = (2, 2, 2)
-        self.block3 = (1, 2, 3)
-        self.block4 = (-4, -4, -4)
-        self.block5 = (4, 4, 4)
-        self.centeredbox1 = (self.block1, self.block2)
-        self.centeredbox2 = (self.block4, self.block5)
-        self.box1 = (self.origin, self.block2)
-        self.box2 = (self.block1, self.block5)
-        self.box3 = (self.block1, self.block4)
+origin = (0, 0, 0)
+block1 = (-2, -2, -2)
+block2 = (2, 2, 2)
+block3 = (1, 2, 3)
+block4 = (-4, -4, -4)
+block5 = (4, 4, 4)
+centered_box_1 = (block1, block2)
+centered_box_2 = (block4, block5)
+box1 = (origin, block2)
+box2 = (block1, block5)
+box3 = (block1, block4)
 
-    def test_block_is_in_box(self):
-        self.assertTrue(boxes.block_is_in_box(self.origin, self.centeredbox1))
-        self.assertFalse(boxes.block_is_in_box(self.block3, (self.block1, self.block2)))
 
-    def test_box_is_in_box(self):
-        self.assertTrue(boxes.box_is_in_box(self.centeredbox1, self.centeredbox2))
-        self.assertTrue(boxes.box_is_in_box(self.box1, self.centeredbox2))
-        self.assertFalse(boxes.box_is_in_box(self.centeredbox2, self.centeredbox1))
-        self.assertFalse(boxes.box_is_in_box(self.box1, self.box3))
+def test_block_is_in_box():
+    assert boxes.block_is_in_box(origin, centered_box_1)
+    assert not (boxes.block_is_in_box(block3, (block1, block2)))
+
+
+def test_box_is_in_box():
+    assert boxes.box_is_in_box(centered_box_1, centered_box_2)
+    assert boxes.box_is_in_box(box1, centered_box_2)
+    assert not boxes.box_is_in_box(centered_box_2, centered_box_1)
+    assert not boxes.box_is_in_box(box1, box3)
--- tests/test_schematics.py
+++ tests/test_schematics.py
@@ -1,157 +1,161 @@
-import unittest
 from litemapy import Schematic, Region, BlockState
 from os import walk
 from constants import *
 import helper
 from tempfile import TemporaryDirectory
 
-class TestAgainstSchematics(unittest.TestCase):
 
-    def setUp(self):
-        self.validfiles = []
-        for dir, dnames, fnames in walk(VALID_LITEMATIC_DIRECTORY):
-            for fname in fnames:
-                self.validfiles.append(dir + "/" + fname)
+valid_files = []
+for directory, child_directory, file_names in walk(VALID_LITEMATIC_DIRECTORY):
+    for file_name in file_names:
+        valid_files.append(directory + "/" + file_name)
 
-    def test_valid_litematics(self):
-        for fname in self.validfiles:
-            schem = Schematic.load(fname)
 
-class TestMaking(unittest.TestCase):
+def test_valid_litematics_do_not_raise_exception_when_loaded():
+    for file_path in valid_files:
+        Schematic.load(file_path)
 
-    def test_size(self):
-        sch = Schematic()
-        self.assertEqual(sch.width, 0)
-        self.assertEqual(sch.height, 0)
-        self.assertEqual(sch.length, 0)
-        reg1 = Region(0, 0, 0, 10, 10, 10)
-        reg2 = Region(90, 0, 0, 10, 10, 10)
-        reg3 = Region(50, 0, 0, 10, 10, 10)
-        reg4 = Region(49, 0, 0, -10, 10, 10)
-        sch.regions["a"] = reg1
-        self.assertEqual(sch.width, 10)
-        self.assertEqual(sch.height, 10)
-        self.assertEqual(sch.length, 10)
-        sch.regions["b"] = reg2
-        self.assertEqual(sch.width, 100)
-        sch.regions["c"] = reg3
-        self.assertEqual(sch.width, 100)
-        del sch.regions["b"]
-        self.assertEqual(sch.width, 60)
-        sch.regions["c"] = reg4
-        self.assertEqual(sch.width, 50)
 
-    def test_reg_min_max(self):
-        reg = Region(0, 0, 0, 10, 20, 30)
-        self.assertEqual(reg.minschemx(), 0)
-        self.assertEqual(reg.maxschemx(), 9)
-        self.assertEqual(reg.minschemy(), 0)
-        self.assertEqual(reg.maxschemy(), 19)
-        self.assertEqual(reg.minschemz(), 0)
-        self.assertEqual(reg.maxschemz(), 29)
-        self.assertEqual(reg.minx(), 0)
-        self.assertEqual(reg.maxx(), 9)
-        self.assertEqual(reg.miny(), 0)
-        self.assertEqual(reg.maxy(), 19)
-        self.assertEqual(reg.minz(), 0)
-        self.assertEqual(reg.maxz(), 29)
-        reg = Region(0, 0, 0, -10, -20, -30)
-        self.assertEqual(reg.minschemx(), -9)
-        self.assertEqual(reg.maxschemx(), 0)
-        self.assertEqual(reg.minschemy(), -19)
-        self.assertEqual(reg.maxschemy(), 0)
-        self.assertEqual(reg.minschemz(), -29)
-        self.assertEqual(reg.maxschemz(), 0)
-        self.assertEqual(reg.minx(), -9)
-        self.assertEqual(reg.maxx(), 0)
-        self.assertEqual(reg.miny(), -19)
-        self.assertEqual(reg.maxy(), 0)
-        self.assertEqual(reg.minz(), -29)
-        self.assertEqual(reg.maxz(), 0)
-        reg = Region(10, 10, 10, 10, 10, 10)
-        self.assertEqual(reg.minschemx(), 10)
-        self.assertEqual(reg.maxschemx(), 19)
-        self.assertEqual(reg.minschemy(), 10)
-        self.assertEqual(reg.maxschemy(), 19)
-        self.assertEqual(reg.minschemz(), 10)
-        self.assertEqual(reg.maxschemz(), 19)
-        self.assertEqual(reg.minx(), 0)
-        self.assertEqual(reg.maxx(), 9)
-        self.assertEqual(reg.miny(), 0)
-        self.assertEqual(reg.maxy(), 9)
-        self.assertEqual(reg.minz(), 0)
-        self.assertEqual(reg.maxz(), 9)
-        reg = Region(-10, -10, -10, 10, 10, 10)
-        self.assertEqual(reg.minschemx(), -10)
-        self.assertEqual(reg.maxschemx(), -1)
-        self.assertEqual(reg.minschemy(), -10)
-        self.assertEqual(reg.maxschemy(), -1)
-        self.assertEqual(reg.minschemz(), -10)
-        self.assertEqual(reg.maxschemz(), -1)
-        self.assertEqual(reg.minx(), 0)
-        self.assertEqual(reg.maxx(), 9)
-        self.assertEqual(reg.miny(), 0)
-        self.assertEqual(reg.maxy(), 9)
-        self.assertEqual(reg.minz(), 0)
-        self.assertEqual(reg.maxz(), 9)
-        reg = Region(-10, -10, -10, -10, -10, -10)
-        self.assertEqual(reg.minschemx(), -19)
-        self.assertEqual(reg.maxschemx(), -10)
-        self.assertEqual(reg.minschemy(), -19)
-        self.assertEqual(reg.maxschemy(), -10)
-        self.assertEqual(reg.minschemz(), -19)
-        self.assertEqual(reg.maxschemz(), -10)
-        self.assertEqual(reg.minx(), -9)
-        self.assertEqual(reg.maxx(), 0)
-        self.assertEqual(reg.miny(), -9)
-        self.assertEqual(reg.maxy(), 0)
-        self.assertEqual(reg.minz(), -9)
-        self.assertEqual(reg.maxz(), 0)
+def test_regions_have_accurate_sizes():
+    sch = Schematic()
+    assert sch.width == 0
+    assert sch.height == 0
+    assert sch.length == 0
+    reg1 = Region(0, 0, 0, 10, 10, 10)
+    reg2 = Region(90, 0, 0, 10, 10, 10)
+    reg3 = Region(50, 0, 0, 10, 10, 10)
+    reg4 = Region(49, 0, 0, -10, 10, 10)
+    sch.regions["a"] = reg1
+    assert sch.width == 10
+    assert sch.height == 10
+    assert sch.length == 10
+    sch.regions["b"] = reg2
+    assert sch.width == 100
+    sch.regions["c"] = reg3
+    assert sch.width == 100
+    del sch.regions["b"]
+    assert sch.width == 60
+    sch.regions["c"] = reg4
+    assert sch.width == 50
 
-    def test_random_write_read(self):
-        dir = TemporaryDirectory()
-        for i in range(100):
-            writeschem = helper.randomschematic()
-            fname = dir.name + "/" + writeschem.name + ".litematic"
-            writeschem.save(fname)
-            readschem = Schematic.load(fname)
-            self.assertEqual(writeschem.name, readschem.name)
-            self.assertEqual(writeschem.author, readschem.author)
-            self.assertEqual(writeschem.description, readschem.description)
-            self.assertEqual(writeschem.width, readschem.width)
-            self.assertEqual(writeschem.height, readschem.height)
-            self.assertEqual(writeschem.length, readschem.length)
-            self.assertEqual(len(writeschem.regions), len(readschem.regions))
-            for name, wreg in writeschem.regions.items():
-                rreg = readschem.regions[name]
-                self.assertEqual(wreg.minx(), rreg.minx())
-                self.assertEqual(wreg.maxx(), rreg.maxx())
-                self.assertEqual(wreg.miny(), rreg.miny())
-                self.assertEqual(wreg.maxy(), rreg.maxy())
-                self.assertEqual(wreg.minz(), rreg.minz())
-                self.assertEqual(wreg.maxz(), rreg.maxz())
-                self.assertEqual(wreg.minschemx(), rreg.minschemx())
-                self.assertEqual(wreg.maxschemx(), rreg.maxschemx())
-                self.assertEqual(wreg.minschemy(), rreg.minschemy())
-                self.assertEqual(wreg.maxschemy(), rreg.maxschemy())
-                self.assertEqual(wreg.minschemz(), rreg.minschemz())
-                self.assertEqual(wreg.maxschemz(), rreg.maxschemz())
-                for x, y, z in wreg.allblockpos():
-                    ws = wreg.getblock(x, y, z)
-                    rs = rreg.getblock(x, y, z)
-                    self.assertEqual(ws, rs)
-        dir.cleanup()
 
-class TestBlockStates(unittest.TestCase):
+def test_region_min_and_max_are_accurate():
+    reg = Region(0, 0, 0, 10, 20, 30)
+    assert reg.minschemx() == 0
+    assert reg.maxschemx() == 9
+    assert reg.minschemy() == 0
+    assert reg.maxschemy() == 19
+    assert reg.minschemz() == 0
+    assert reg.maxschemz() == 29
+    assert reg.minx() == 0
+    assert reg.maxx() == 9
+    assert reg.miny() == 0
+    assert reg.maxy() == 19
+    assert reg.minz() == 0
+    assert reg.maxz() == 29
+    reg = Region(0, 0, 0, -10, -20, -30)
+    assert reg.minschemx() == -9
+    assert reg.maxschemx() == 0
+    assert reg.minschemy() == -19
+    assert reg.maxschemy() == 0
+    assert reg.minschemz() == -29
+    assert reg.maxschemz() == 0
+    assert reg.minx() == -9
+    assert reg.maxx() == 0
+    assert reg.miny() == -19
+    assert reg.maxy() == 0
+    assert reg.minz() == -29
+    assert reg.maxz() == 0
+    reg = Region(10, 10, 10, 10, 10, 10)
+    assert reg.minschemx() == 10
+    assert reg.maxschemx() == 19
+    assert reg.minschemy() == 10
+    assert reg.maxschemy() == 19
+    assert reg.minschemz() == 10
+    assert reg.maxschemz() == 19
+    assert reg.minx() == 0
+    assert reg.maxx() == 9
+    assert reg.miny() == 0
+    assert reg.maxy() == 9
+    assert reg.minz() == 0
+    assert reg.maxz() == 9
+    reg = Region(-10, -10, -10, 10, 10, 10)
+    assert reg.minschemx() == -10
+    assert reg.maxschemx() == -1
+    assert reg.minschemy() == -10
+    assert reg.maxschemy() == -1
+    assert reg.minschemz() == -10
+    assert reg.maxschemz() == -1
+    assert reg.minx() == 0
+    assert reg.maxx() == 9
+    assert reg.miny() == 0
+    assert reg.maxy() == 9
+    assert reg.minz() == 0
+    assert reg.maxz() == 9
+    reg = Region(-10, -10, -10, -10, -10, -10)
+    assert reg.minschemx() == -19
+    assert reg.maxschemx() == -10
+    assert reg.minschemy() == -19
+    assert reg.maxschemy() == -10
+    assert reg.minschemz() == -19
+    assert reg.maxschemz() == -10
+    assert reg.minx() == -9
+    assert reg.maxx() == 0
+    assert reg.miny() == -9
+    assert reg.maxy() == 0
+    assert reg.minz() == -9
+    assert reg.maxz() == 0
 
-    def test(self):
-        prop = {"test1": "testval", "test2": "testval2"}
-        b = BlockState("minecraft:stone", properties=prop)
-        self.assertEqual(len(prop), len(b))
-        for k, v in prop.items():
-            self.assertEqual(b[k], v)
-        nbt = b._tonbt()
-        print(nbt)
-        b2 = BlockState.fromnbt(nbt)
-        self.assertEqual(b, b2)
 
+def test_are_random_schematics_preserved_when_reading_and_writing():
+    temporary_directory = TemporaryDirectory()
+    for i in range(100):
+        write_schematic = helper.randomschematic()
+        file_path = temporary_directory.name + "/" + write_schematic.name + ".litematic"
+        write_schematic.save(file_path)
+        read_schematic = Schematic.load(file_path)
+
+        # Assert metadata is equals
+        assert write_schematic.name == read_schematic.name
+        assert write_schematic.author == read_schematic.author
+        assert write_schematic.description == read_schematic.description
+        assert write_schematic.width == read_schematic.width
+        assert write_schematic.height == read_schematic.height
+        assert write_schematic.length == read_schematic.length
+        assert len(write_schematic.regions) == len(read_schematic.regions)
+        for name, write_region in write_schematic.regions.items():
+            read_region = read_schematic.regions[name]
+
+            # Assert computed values are equal
+            assert write_region.minx() == read_region.minx()
+            assert write_region.maxx() == read_region.maxx()
+            assert write_region.miny() == read_region.miny()
+            assert write_region.maxy() == read_region.maxy()
+            assert write_region.minz() == read_region.minz()
+            assert write_region.maxz() == read_region.maxz()
+            assert write_region.minschemx() == read_region.minschemx()
+            assert write_region.maxschemx() == read_region.maxschemx()
+            assert write_region.minschemy() == read_region.minschemy()
+            assert write_region.maxschemy() == read_region.maxschemy()
+            assert write_region.minschemz() == read_region.minschemz()
+            assert write_region.maxschemz() == read_region.maxschemz()
+
+            # Assert all blocks are equal
+            for x, y, z in write_region.allblockpos():
+                ws = write_region.getblock(x, y, z)
+                rs = read_region.getblock(x, y, z)
+                assert ws == rs
+
+    temporary_directory.cleanup()
+
+
+def test_blockstate():
+    # TODO Split into multiple smaller tests
+    prop = {"test1": "testval", "test2": "testval2"}
+    b = BlockState("minecraft:stone", properties=prop)
+    assert len(prop) == len(b)
+    for k, v in prop.items():
+        assert b[k] == v
+    nbt = b._tonbt()
+    b2 = BlockState.fromnbt(nbt)
+    assert b == b2
