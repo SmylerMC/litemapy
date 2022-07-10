32703674b51efdf38278c66c6572f9ac29af61a8
Jack <DevJackMC@gmail.com>
2022-07-10 10:29:40 -0700

    before

--- litemapy/__init__.py
+++
@@ -1,6 +0,0 @@
-from .Schematic import Schematic
-from .Region import Region
-from .BlockState import BlockState
-from .Entity import Entity
-from.TileEntity import TileEntity
-from .info import LITEMAPY_VERSION as __version__
--- litemapy/info.py
+++
@@ -1,6 +0,0 @@
-LITEMATIC_VERSION = 6  # Litematic version we are trying to imitate from Litematica (1.18.2-0.11.3)
-SPONGE_VERSION = 2  # Sponge format version used by WorldEdit (https://github.com/SpongePowered/Schematic-Specification)
-MC_DATA_VERSION = 2975  # Minecraft 1.18.2 (https://minecraft.fandom.com/wiki/Data_version)
-DEFAULT_NAME = "Unnamed"  # Default name given to schematics and regions if unspecified
-LITEMAPY_NAME = "Litemapy"  # Used to identify schematic created with Litemapy in metadata
-LITEMAPY_VERSION = "0.7.0b0"  # Unique version string
