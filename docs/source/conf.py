24ecdf57456586cc34d6c26edee0da56e5c634b2
SmylerMC <smyler@mail.com>
2022-07-26 00:32:59 +0200

    Read documentation version from project files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,32 +1,15 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
+import re
 
 project = 'Litemapy'
 copyright = '2022, Smyler and Litemapy contributors'
 author = 'Smyler and Litemapy contributors'
 
+# Read version from project files,
+# we don't import it like we do in setup.py because RTD takes care of it and we don't want to interfere
+with open("../../litemapy/info.py") as f:
+    match = re.search(r'LITEMAPY_VERSION = "(.+)"', f.read())
+    version = match[1]
 
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
@@ -42,26 +25,12 @@ intersphinx_mapping = {
 }
 intersphinx_disabled_domains = ['std']
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # Include __init__ doc in class doc
 autoclass_content = 'both'
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
