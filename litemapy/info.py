196cf0c1402ea922aa5b026ea9d5b397aa6cab96
Smyler <smyler@mail.com>
2024-06-06 21:48:04 +0200

    Preserve litematic subversion

--- litemapy/info.py
+++ litemapy/info.py
@@ -1,4 +1,5 @@
-LITEMATIC_VERSION = 6  # Litematic version we are trying to imitate from Litematica (1.18.2-0.11.3)
+LITEMATIC_VERSION = 6
+LITEMATIC_SUBVERSION = 1
 SPONGE_VERSION = 2  # Sponge format version used by WorldEdit (https://github.com/SpongePowered/Schematic-Specification)
 MC_DATA_VERSION = 2975  # Minecraft 1.18.2 (https://minecraft.wiki/w/Data_version)
 DEFAULT_NAME = "Unnamed"  # Default name given to schematics and regions if unspecified
